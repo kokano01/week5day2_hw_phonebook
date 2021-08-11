37e797fea9357a0d478f4bc46171d8e000ab76b4
kokano01 <kokiokano@hotmail.com>
2021-08-10 22:17:06 -0700

    2nd

--- myphonebook/blog/urls.py
+++ myphonebook/blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
+
 from . import views
 
 urlpatterns = [
     path('',  views.index, name = 'blog-index'),
-    path('posts/', views.posts, name = 'blog-posts'),
+    path('phonebook/', views.posts, name = 'blog-posts'),
     path('posts/create',  views.createPost, name = 'blog-createpost'),
     path('register/',  views.registerPage, name = 'blog-register'),
     path('login/',  views.loginPage, name = 'blog-login'),
