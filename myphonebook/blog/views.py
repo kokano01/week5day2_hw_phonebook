c95454e787d8b3f94937b3f821aee30fce821f8e
kokano01 <kokiokano@hotmail.com>
2021-08-11 22:06:58 -0700

    updated for week5day3 hw: added login required and email on signup

--- myphonebook/blog/views.py
+++ myphonebook/blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, redirect
+
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
+
 from .forms import PostForm, UserForm
 from .models import Post
+
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
 
 # Create your views here.
 def index(request):
@@ -27,11 +36,31 @@ def registerPage(request):
     form = UserForm(request.POST or None)
     if form.is_valid():
         form.save()
+
+        user = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+
+        context = {'username': user}
+        template = render_to_string('blog/emailtemplate.html', context)
+
+        email_message = EmailMessage(
+            'Welcome to my Django phonebook',  # subject line
+            template, # body
+            settings.EMAIL_HOST_USER,
+            [email]
+        )
+
+        email_message.fail_silently = False
+        email_message.send()
+
+        return redirect('blog-login')
     context = {
         'form': form
     }
     return render(request, 'blog/register.html', context)
 
+
+@login_required(login_url='blog-login')
 def createPost(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
@@ -53,6 +82,8 @@ def loginPage(request):
             return redirect('blog-index')
     return render(request, 'blog/login.html')
 
+
+@login_required(login_url='blog-login')
 def logoutUser(request):
     logout(request)
     return redirect('blog-login')
